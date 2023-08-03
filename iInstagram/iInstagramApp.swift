c4cfe1190b4156ae95b2b866f97fcdb112b268a9
Khondakar Afridi <afridi.khondakar@gmail.com>
2023-08-03 15:42:05 +0600

    Initial Commit

---
+++ iInstagram/iInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  iInstagramApp.swift
+//  iInstagram
+//
+//  Created by Khondakar Afridi on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct iInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
